709b496603fcb908a0b2ae4d261fa055fda112c7
kaili chu <kailichu14@gmail.com>
2018-08-03 10:51:43 -0700

    trivia implemented

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ $(document).ready(function(){
   $('#rotd-button-hide').hide();
   $('.rd-answer').hide();
   $('.rd-button-hide').hide();
+  $('.tr-answer').hide();
+  $('.tr-button-hide').hide();
+  $('.trivia-questions').hide();
 
   // NOTE: RIDDLE-OF-THE-DAY CONTROLS
   // NOTE: Shows ROTD answer
@@ -45,8 +48,32 @@ $(document).ready(function(){
     $rdAnswer.hide();
   })
 
+  // NOTE: TRIVIA CONTROLS
+  // NOTE: Shows TR answer
+  $('.tr-button-show').click(function(){
+    $(this).hide();
+    var $triviaBox = $(this).parent();
+    var $buttonHide = $triviaBox.find('.tr-button-hide')
+    var $trAnswer = $triviaBox.find('.tr-answer');
+    $buttonHide.show();
+    $trAnswer.show();
+  })
 
+  // NOTE: Hide TR answer
+  $('.tr-button-hide').click(function(){
+    $(this).hide();
+    var $triviaBox = $(this).parent();
+    var $buttonShow = $triviaBox.find('.tr-button-show')
+    var $trAnswer = $triviaBox.find('.tr-answer');
+    $buttonShow.show();
+    $trAnswer.hide();
+  })
 
+  // NOTE: Hides pictures when 'Questions' button is clicked.
+  $('#tr-questions').click(function(){
+    $('.trivia-questions').show();
+    $('#picture-box').hide();
+  })
 
   $('.info-box').click(function(){
     var colorArray = ["red", "green", "blue", "orange", "purple", "yellow"];
